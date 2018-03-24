c2ce8971f92a88d58324e620a643a0c570385e4a
Russell Cattelan <cattelan@thebarn.com>
2018-03-24 01:32:07 -0500

    more notes

--- iRedMail/build.sh
+++ iRedMail/build.sh
@@ -2,3 +2,8 @@
 #(cd fbsd_ports; env FLAVORS=py27 __MAKE_CONF=`pwd`/../fbsd_make.conf BATCH= PACKAGE_BUILDING= make fetch-recursive)
 (cd fbsd_ports;  env FLAVORS=py27 WITH_CCACHE_BUILD=yes __MAKE_CONF=`pwd`/../fbsd_make.conf BATCH= PACKAGE_BUILDING= FORCE_PKG_REGISTER= make all)
 (cd fbsd_ports;  env FLAVORS=py27 WITH_CCACHE_BUILD=yes __MAKE_CONF=`pwd`/../fbsd_make.conf BATCH= PACKAGE_BUILDING= FORCE_PKG_REGISTER= make package-recursive)
+#
+#pw groupadd -g 6971 cattelan
+#pw useradd -s /bin/tcsh -u 6971 -g cattelan -L root -n cattelan -m -w yes -c "Russell Cattelan"
+#pw groupmod -n wheel -m cattelan
+#pw groupmod -n freebsd -m cattelan
